378eb2563bdf945d01caa22e8310166a50ad6a61
Marcus Sandes <marcus.sandesf@gmail.com>
2025-08-02 12:50:48 -0300

    final - melhorias na apresentação no terminal

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,21 +16,81 @@ public class Main {
         curso2.setDescricao("Descrição curso JavaScript");
         curso2.setCargaHoraria(4);
 
-        Curso curso3 = new Curso();
-        curso3.setTitulo("Curso C++");
-        curso3.setDescricao("Descrição curso C++");
-        curso3.setCargaHoraria(6);
-
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição mentoria de Java");
         mentoria.setData(LocalDate.now());
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
-        System.out.println();
+
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+//        System.out.println();
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMarcus = new Dev();
+        devMarcus.setNome("Marcus");
+        devMarcus.inscreverBootcamp(bootcamp);
+        System.out.println("\uD83D\uDC68\u200D\uD83D\uDCBB Desenvolvedor: " + devMarcus.getNome());
+        System.out.println("📅 Data de inscrição : " + LocalDate.now().format(formatter));
+        System.out.println("-------------------------------------------");
+        System.out.println("\uD83D\uDCDA Conteúdos inscritos: ");
+        if(devMarcus.getConteudosInscritos().isEmpty()){
+            System.out.println("Nenhum conteúdo inscrito.");
+        }else {
+            devMarcus.getConteudosInscritos()
+                    .forEach(c -> System.out.println(" -" + c.getTitulo()));
+        }
+
+        System.out.println("\n✅ Conteúdos concluidos: ");
+        if (devMarcus.getConteudosConcluidos().isEmpty()){
+            System.out.println("Nenhum conteúdo concluido.");
+        }else {
+            devMarcus.getConteudosConcluidos()
+                    .forEach(c -> System.out.println(c.getTitulo()));
+        }
+        System.out.println("XP: " + devMarcus.calcularTotalXp());
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.println("\n");
+
+        Dev devJessica = new Dev();
+        devJessica.setNome("Jessica");
+        devJessica.inscreverBootcamp(bootcamp);
+        System.out.println("=================================");
+        System.out.println("\uD83D\uDC68\u200D\uD83D\uDCBB Desenvolvedor: " + devJessica.getNome());
+        System.out.println("📅 Data de inscrição : " + LocalDate.now().format(formatter));
+        System.out.println("---------------------------------------");
+        System.out.println("\uD83D\uDCDA Conteúdos inscritos: ");
+        if(devJessica.getConteudosInscritos().isEmpty()){
+            System.out.println("Nenhum conteúdo inscrito.");
+        }else {
+            devJessica.getConteudosInscritos()
+                    .forEach(c -> System.out.println(" -" + c.getTitulo()));
+        }
+
+        devJessica.progredir();
+        devJessica.progredir();
+        devJessica.progredir();
+
+        System.out.println("\n✅ Conteudos Concluidos: ");
+        if (devJessica.getConteudosConcluidos().isEmpty()) {
+            System.out.println(" Nenhum conteúdo concluido!");
+        }else {
+            devJessica.getConteudosConcluidos()
+                    .forEach(c -> System.out.println(" -" + c.getTitulo()));
+        }
+
+        System.out.println("\n⭐ XP Total: " + devJessica.calcularTotalXp());
+        System.out.println("==========================================\n");
 
     }
 }
